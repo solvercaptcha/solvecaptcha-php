a0c60eb3d5ca7baf29f4090faad10800726fa5bc
solvecaptcha-com <info@solvecaptcha.com>
2026-02-27 15:12:36 +0300

    googlekey added

--- examples/recaptcha_v2_options.php
+++ examples/recaptcha_v2_options.php
@@ -5,16 +5,15 @@ set_time_limit(610);
 require(__DIR__ . '/../src/autoloader.php');
 
 $solver = new \SolveCaptcha\SolveCaptcha([
-    'apiKey'	=> 'YOUR_API_KEY',
+    'apiKey'	=> $argv[1], //YOUR_API_KEY
     'server'	=> 'https://solvecaptcha.com'
 ]);
 
 try {
     $result = $solver->recaptcha([
-        'sitekey'   => '6Le-wvkSVVABCPBMRTvw0Q4Muexq1bi0DJwx_mJ-',
+        'googlekey' => '6LfB5_IbAAAAAMCtsjEHEHKqcB9iQocwwxTiihJu',
         'url'       => 'https://mysite.com/page/with/recaptcha',
         'invisible' => 1,
-        'action'    => 'verify',
         'proxy'     => [
             'type' => 'HTTPS',
             'uri'  => 'login:password@IP_address:PORT',
