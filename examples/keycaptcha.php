bff33dd5bbfd347a0a9be4ba015dac18ffb20985
solvecaptcha-com <info@solvecaptcha.com>
2026-03-03 12:47:59 +0300

    pageurl added

--- examples/keycaptcha.php
+++ examples/keycaptcha.php
@@ -12,7 +12,7 @@ try {
         's_s_c_session_id'       => '493e52c37c10c2bcdf4a00cbc9ccd1e8',
         's_s_c_web_server_sign'  => '9006dc725760858e4c0715b835472f22-pz-',
         's_s_c_web_server_sign2' => '2ca3abe86d90c6142d5571db98af6714',
-        'url'                    => 'https://www.keycaptcha.ru/demo-magnetic/',
+        'pageurl'                => 'https://www.keycaptcha.ru/demo-magnetic/',
     ]);
 } catch (\Exception $e) {
     die($e->getMessage());
