15ea14a162e490ccf95c75ad4c7cd9c179eea944
solvecaptcha-com <info@solvecaptcha.com>
2026-03-05 09:30:43 +0300

    coordinates upd

--- examples/coordinates_options.php
+++ examples/coordinates_options.php
@@ -13,8 +13,7 @@ try {
     $result = $solver->coordinates([
         'file'     => __DIR__ . '/images/grid_2.jpg',
         'language' => 2,
-     // 'hintImg'  => __DIR__ . '/images/grid_hint.jpg'
-        'hintText' => 'Select all images with an Orange',
+        'textinstructions' => 'Select all images with an Orange',
     ]);
 } catch (\Exception $e) {
     die($e->getMessage());
